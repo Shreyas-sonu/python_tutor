58c67552224063b1d02aa9adf537785b40b92305
Shreyas-sonu <sonueigns007@gmail.com>
2023-07-13 19:15:43 +0530

    input

---
+++ basic/input.py
@@ -0,0 +1,7 @@
+a=input("Enter your Name") #fx for taking input
+print(a)
+#using concatenation to print hello with the name entered by user
+print("hello ",a)
+print("hello "+a)
+# Format String
+print(f"hello {a}")
---
+++ basic/print.py
@@ -0,0 +1,6 @@
+print("hello world")
+# syntax print(object,sep=''.end='\n')
+a=123
+b="hello"
+print (a,b,sep="xxx",end=" next ")
+print (a,b,sep="yyy")
